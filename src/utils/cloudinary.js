6d1e8948a3cf8d0504b9bd47d30aba09406d1bbe
irishabhnegi <rishabhnegi175@gmail.com>
2024-02-29 17:13:33 +0530

    fic all register controller bugs

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,12 +1,10 @@
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
 
-import { v2 as cloudinary } from "cloudinary";
-
 cloudinary.config({
-  cloud_name: PROCESS.ENV.CLOUDINARY_CLOUD_NAME,
-  api_key: PROCESS.ENV.CLOUDINARY_API_KEY,
-  api_secret: PROCESS.ENV.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const uploadOnCloudinary = async (localFilePath) => {
@@ -18,7 +16,8 @@ const uploadOnCloudinary = async (localFilePath) => {
     });
 
     // file has been uploaded successfully
-    console.log("file is uploaded on cloudinary", response.url);
+    // console.log("file is uploaded on cloudinary", response.url);
+    fs.unlinkSync(localFilePath);
     return response;
   } catch (error) {
     fs.unlinkSync(localFilePath); // remove the locally saved temp. file as the upload operation got failed
